d4920c409dc1a99c6efc654dd220dbe593e52bba
Par Eriksson <parer800@student.liu.se>
2014-03-31 16:53:39 +0200

    Constructed TimeBlock class and documentation for it'

--- src/src/tson_utilities/TimeBlock.java
+++ src/src/tson_utilities/TimeBlock.java
@@ -1,5 +1,86 @@
 package tson_utilities;
 
-public class TimeBlock {
+import java.util.Calendar;
 
+public class TimeBlock 
+{
+	private Calendar date;
+	private int hours;
+	private int minutes;
+	
+	public TimeBlock(Calendar theDate, int theHours, int theMinutes)
+	{
+		setTimeBlock(theDate, theHours, theMinutes);
+	}
+	/**
+	 * 
+	 * @param theDate - Java Calendar type
+	 * @param theHours - hours
+	 * @param theMinutes - minutes
+	 */
+	public void setTimeBlock(Calendar theDate, int theHours, int theMinutes)
+	{
+		date = theDate;
+		hours = theHours;
+		minutes = theMinutes;
+	}
+	/**
+	 * 
+	 * @param d - Java Calendar type
+	 */
+	public void setDate(Calendar d)
+	{
+		date = d;
+	}
+	/**
+	 * 
+	 * @param h - hours
+	 */
+	public void setHours(int h)
+	{
+		hours = h;
+	}
+	/**
+	 * 
+	 * @param m - minutes
+	 */
+	public void setMinutes(int m)
+	{
+		minutes = m;
+	}
+	/**
+	 * 
+	 * @param h - hours
+	 * @param m - minutes
+	 */
+	public void setDuration(int h, int m)
+	{
+		setHours(h);
+		setMinutes(m);
+	}
+	/**
+	 * 
+	 * @return time of TimeBlock in minutes
+	 */
+	public int getTimeInMinutes()
+	{
+		return hours*60 + minutes;
+	}
+	/**
+	 * 
+	 * @return time of TimeBlock as String
+	 */
+	public String getTimeAsString()
+	{
+		return hours+"h"+minutes+"m";
+	}
+	/**
+	 * 
+	 * @return the date as Calendar type
+	 */
+	public Calendar getDate()
+	{
+		return date;
+	}
+	
 }
