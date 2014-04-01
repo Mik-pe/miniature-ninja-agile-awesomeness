4c658ab657de8084487c4b6aaaa37f8a354ce5da
Magrit <mikpe201@student.liu.se>
2014-04-01 11:31:14 +0200

    Working javafiles

--- src/src/tson_utilities/User.java
+++ src/src/tson_utilities/User.java
@@ -1,5 +1,6 @@
 package tson_utilities;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class User 
@@ -7,13 +8,23 @@ public class User
 	 private String email = "";
 	 private String name = ""; 
 	 private String id = "";
-	 private List<Project> projectList = null;
+	 private List<Project> projectList = new ArrayList<Project>();
 	 
 	 public User(String email, String name, String id)
 	 {
 		 this.email = email;
 		 this.name = name;
 		 this.id = id;
+		
+		 Project p1 = new Project("SafariResa");
+		 Project p2 = new Project("IndienResa");
+		 Project p3 = new Project("Resa till USA 'MURRICA");
+		 
+		 this.addProject(p1);
+		 this.addProject(p2);
+		 this.addProject(p3);
+		
+		 
 	 }
 	 
 	 public void addProject(Project p)
