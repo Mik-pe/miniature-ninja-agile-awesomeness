2e99d7a29d59a72308a0374dd9452a7b568192d8
sofli222 <sofli222@student.liu.se>
2014-04-01 09:47:26 +0200

    Create Project button added on homescreen

--- src/src/com/example/tson/HomeActivity.java
+++ src/src/com/example/tson/HomeActivity.java
@@ -2,12 +2,12 @@ package com.example.tson;
 
 //IMPORT OUR OWN CLASSES
 import tson_utilities.*;
-
 //IMPORT ANDROID
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 
 //IMPORT OTHER
 
@@ -27,6 +27,21 @@ public class HomeActivity extends Activity
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.home, menu);
         return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.:
+                openSearch();
+                return true;
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
     
 }
