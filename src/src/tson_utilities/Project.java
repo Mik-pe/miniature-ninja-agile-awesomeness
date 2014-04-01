45f8349972e452f26f8ffc629aa65d40514c7559
Magrit <mikpe201@student.liu.se>
2014-04-01 08:37:02 +0200

    DOCUMENTATION

--- src/src/tson_utilities/Project.java
+++ src/src/tson_utilities/Project.java
@@ -4,33 +4,47 @@ import java.util.Calendar;
 import java.util.List;
 
 /**
- * 
+ * Project class is used as an object for each project created
+ * by the user. User can add time, get time and get the name
+ * of the project.
  * @author mikpe201
- *Project class is used as an object for each project created
- *by the user. User can add time, get time and get the name
- *of the project.
+ *
  */
 public class Project {
 	
-private String name;
-private List<TimeBlock> submissionList;
-
-/**
- * 
- * @param d - date
- * @param h - hours
- * @param m - minutes
- */
-	public void addTime(Calendar d, int h, int m)
+	private String name;
+	private List<TimeBlock> submissionList;
+	
+	/**
+	 * Constructor for Project Class, submissions are added
+	 * at a later point.
+	 * @param name Adds name to Project
+	 */
+	public Project(String name)
 	{
-		TimeBlock t = new TimeBlock(d, h, m);
-		submissionList.add(t);
+		this.name = name;
 	}
 	
 	/**
-	 * 
+	 * Adds a TimeBlock to a project
 	 * @param d - date
-	 * @return - returns string of amount of hours of that 
+	 * @param h - hours
+	 * @param m - minutes
+	 */
+	public void addTime(Calendar d, int h, int m)
+	{
+		
+		TimeBlock t = new TimeBlock(d, h, m);
+		if(!submissionList.contains(t))
+		{
+			submissionList.add(t);
+		}
+	}
+	
+	/**
+	 * Getter of string
+	 * @param d  date the time should correspond to
+	 * @return  the string of amount of hours of that 
 	 * date and project
 	 */
 	public String getTimeByDate(Calendar d)
@@ -38,18 +52,21 @@ private List<TimeBlock> submissionList;
 		
 		for(int i=this.submissionList.size(); i>0 ;i--)
 		{
-			if(this.submissionList.get(i).getDate() == d){
+			if(this.submissionList.get(i).getDate() == d)
+			{
 				return this.submissionList.get(i).getTimeAsString();
 			}
 		}
 		
 		return "-";
 	}
+	
 	/**
-	 * 
-	 * @return - returns the name of the project.
+	 * Getter
+	 * @return  a String of the name of the project.
 	 */
-	public String getName(){
+	public String getName()
+	{
 		return name;
 	}
 	
