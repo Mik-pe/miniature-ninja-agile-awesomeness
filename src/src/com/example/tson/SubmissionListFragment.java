2d566ddb2f7a92736673a5c01d2669c5aabba113
Magrit <mikpe201@student.liu.se>
2014-04-29 10:39:09 +0200

    Layoutfiiixx

--- src/src/com/example/tson/SubmissionListFragment.java
+++ src/src/com/example/tson/SubmissionListFragment.java
@@ -98,8 +98,11 @@ public class SubmissionListFragment extends Fragment {
     		final SubmissionDayListItem currentItem = subList.get(position);
     		TextView submissionDate = (TextView) view.findViewById(R.id.submissionDate);
     		TextView projectTime = (TextView) view.findViewById(R.id.workTime);
-    		ImageButton editButton = (ImageButton) view.findViewById(R.id.editDayButton);	
+    		View orangeBelow = (View) view.findViewById(R.id.belowWeek);
+    		View orangeAbove = (View) view.findViewById(R.id.aboveWeek);
     		TextView weekText = (TextView) view.findViewById(R.id.weekText);
+    		ImageButton editButton = (ImageButton) view.findViewById(R.id.editDayButton);
+    		
     		/**
     		 * Will check if this is the first SubMissionListItem or a SUNDAY
     		 * IF: 		Will write the week number.
@@ -107,30 +110,25 @@ public class SubmissionListFragment extends Fragment {
     		 */
     		if(position == 0 || currentItem.today.get(Calendar.DAY_OF_WEEK)==Calendar.SUNDAY)
     		{
+    			orangeAbove.setVisibility(View.VISIBLE);
     			weekText.setVisibility(TextView.VISIBLE);
     			weekText.setText("Week: "+currentItem.today.get(Calendar.WEEK_OF_YEAR));
-        		
-    			RelativeLayout.LayoutParams params =  (RelativeLayout.LayoutParams)submissionDate.getLayoutParams();
-    			params.addRule(RelativeLayout.ALIGN_PARENT_TOP, 0);
-    			params.addRule(RelativeLayout.BELOW, R.id.weekText);
-    			
-    			RelativeLayout.LayoutParams paramsButton =  (RelativeLayout.LayoutParams)editButton.getLayoutParams();
-    			paramsButton.addRule(RelativeLayout.BELOW, R.id.weekText);
+    			orangeBelow.setVisibility(View.VISIBLE);
     		}
     		else
     		{
+    			orangeAbove.setVisibility(View.GONE);
     			weekText.setVisibility(TextView.GONE);
     			weekText.setText("");
-    			RelativeLayout.LayoutParams params =  (RelativeLayout.LayoutParams)submissionDate.getLayoutParams();
-    			params.addRule(RelativeLayout.ALIGN_PARENT_TOP, 0);
-    			params.addRule(RelativeLayout.BELOW, 0);
-    			
-    			RelativeLayout.LayoutParams paramsButton =  (RelativeLayout.LayoutParams)editButton.getLayoutParams();
-    			paramsButton.addRule(RelativeLayout.BELOW, 0);
+    			orangeBelow.setVisibility(View.GONE);
     		}
     		
     		submissionDate.setText(currentItem.today.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.ENGLISH)+" "+currentItem.today.get(Calendar.DAY_OF_MONTH)+"/"+(currentItem.today.get(Calendar.MONTH)+1));
-    		
+    		/**
+    		 * Will check if the date is SATURDAY or SUNDAY
+    		 * IF: Set text to orange-ish
+    		 * ELSE: Dark-grey
+    		 */
     		if(currentItem.today.get(Calendar.DAY_OF_WEEK)== Calendar.SATURDAY||currentItem.today.get(Calendar.DAY_OF_WEEK)==Calendar.SUNDAY)
 			{
 				submissionDate.setTextColor(getResources().getColor(R.color.combitech_orange));
