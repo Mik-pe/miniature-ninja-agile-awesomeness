fb13ec3181ddfcf076e02177c74a06a228e3de91
tobiasjr <tobjo423@student.liu.se>
2014-05-06 09:58:11 +0200

    startup page

--- src/src/com/example/tson/LoginActivity.java
+++ src/src/com/example/tson/LoginActivity.java
@@ -2,21 +2,34 @@ package com.example.tson;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class LoginActivity extends Activity {
-
+	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) 
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.login, menu);
-		return true;
+		final Intent intent = new Intent(this, HomeActivity.class);
+		
+		ImageButton loginBtn = (ImageButton) findViewById(R.id.login_button);
+		loginBtn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				startActivity(intent);
+				finish();
+				// TODO Auto-generated method stub
+				
+			}
+		});
+    	
+    	
 	}
 
 }
